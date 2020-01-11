a31f50208b1a183a5280694f39d30ee45026c61c
logan gunn <lgunn4@uwo.ca>
2020-01-10 20:45:28 -0500

    initial react app commit

---
+++ src/Content.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Content.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Switch} from "react-router-dom";
+
+function Content() {
+    return (
+        <main>
+            <div className='content'>
+            <Switch>
+            </Switch>
+            </div>
+        </main>
+    );
+}
+
+export default Content;
---
+++ src/assets/constants.js
@@ -0,0 +1,2 @@
+export const applicationName = 'Ulternative';
+export const applicationDescription = 'Welcome to the only online platform that makes purchasing eco friendly alternatives the main focus.';
