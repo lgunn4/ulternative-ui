ee5457658caa3d55514b9ef4ce4966ff1fe5dcdf
logan gunn <lgunn4@uwo.ca>
2020-01-11 22:02:41 -0500

    Created a homepage

--- src/components/FrontPage/FrontPage.jsx
+++ src/components/FrontPage/FrontPage.jsx
@@ -5,62 +5,32 @@ import Button from "react-bootstrap/Button";
 import {applicationDescription, applicationName} from "../../assets/constants";
 import {Link} from "react-router-dom";
 import {mockCategoryData} from "../../assets/MockData/MockData";
-import CategoryCardDeck from "../CategoryCardDeck/CategoryCardDeck";
-import Carousel from "react-bootstrap/Carousel";
-import CardImage from "../../assets/cardImage.svg";
+import CategoryCardDeck from "./CategoryCardDeck";
+import Card from "react-bootstrap/Card"
+import FrontPageBackground from '../../assets/background.jpg';
 
 const FrontPage = () => {
     return (
         <div>
-            <Jumbotron className="front-page-call-to-action">
-                <h1>Welcome to {applicationName}</h1>
-                <p>
-                    {applicationDescription}
-                </p>
-                <p>
-                    <Link exact to="/search" >
-                        <Button variant="success" href="/search">Start Searching</Button>
-                    </Link>
-                </p>
-            </Jumbotron>
+            <Card className="bg-dark text-black">
+                <Card.Img src={FrontPageBackground} className="front-page-image-card"/>
+                <Card.ImgOverlay>
+                    <Jumbotron className="front-page-call-to-action">
+                        <h1>Welcome to {applicationName}</h1>
+                        <p>
+                            {applicationDescription}
+                        </p>
+                        <p>
+                            <Link exact to="/search" >
+                                <Button variant="success" href="/search">Start Searching</Button>
+                            </Link>
+                        </p>
+                    </Jumbotron>
+                </Card.ImgOverlay>
+            </Card>
+
             <CategoryCardDeck categories={mockCategoryData.slice(0,4)} />
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={CardImage}
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={CardImage}
-                        alt="Third slide"
-                    />
 
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={CardImage}
-                        alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
         </div>
 
     );
